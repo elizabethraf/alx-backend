298b5bd10cd762f015304f60e71275d8adb7cc48
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-18 18:51:07 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,9 +5,6 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """Display Basic Dictionary"""
-    def __init__(self):
-        """Return the value in self.cache_data"""
-        super().__init__()
 
     def put(self, key, item):
         if key is not None and item is not None:
