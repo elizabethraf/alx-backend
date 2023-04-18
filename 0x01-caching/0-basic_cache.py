62a76b13db2d30531ddb2188cc88b1bd84417867
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-18 18:54:08 +0000

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,6 +12,5 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Gets cache"""
-        if key is None or key not in self.cache_data:
-            return None
+
         return self.cache_data.get(key, None)
