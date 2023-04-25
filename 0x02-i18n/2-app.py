02e40c00d05b7a70fadc1deeea30ebd1ac80a32f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-25 16:13:00 +0000

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,4 +19,5 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Display Hellow world"""
+    return render_template('1-index.html')
